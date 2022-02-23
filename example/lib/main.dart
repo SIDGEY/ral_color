0dc636ff9f0917a60cdc1b01adb81cac592f0e66
maxvyr <maxvid30@gmail.com>
2022-02-23 21:55:25 +0100

    :lipstick : design example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:example/view/color_ral_view_eight_thousand.dart';
+import 'package:example/view/color_ral_view_five_thousand.dart';
+import 'package:example/view/color_ral_view_four_thousand.dart';
+import 'package:example/view/color_ral_view_nine_thousand.dart';
 import 'package:example/view/color_ral_view_one_thousand.dart';
+import 'package:example/view/color_ral_view_seven_thousand.dart';
+import 'package:example/view/color_ral_view_six_thousand.dart';
 import 'package:example/view/color_ral_view_three_thousands.dart';
 import 'package:example/view/color_ral_view_two_thousands.dart';
 import 'package:example/widget/text_presentation.dart';
@@ -33,7 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.only(
+          top: 20.0,
+          bottom: 20.0,
+        ),
         child: ListView(
           shrinkWrap: true,
           children: const [
@@ -43,6 +52,18 @@ class _MyHomePageState extends State<MyHomePage> {
             ColorView2000(),
             TextPresentation(title: "RAL 3000"),
             ColorView3000(),
+            TextPresentation(title: "RAL 4000"),
+            ColorView4000(),
+            TextPresentation(title: "RAL 5000"),
+            ColorView5000(),
+            TextPresentation(title: "RAL 6000"),
+            ColorView6000(),
+            TextPresentation(title: "RAL 7000"),
+            ColorView7000(),
+            TextPresentation(title: "RAL 8000"),
+            ColorView8000(),
+            TextPresentation(title: "RAL 9000"),
+            ColorView9000(),
           ],
         ));
   }
--- example/lib/view/color_ral_view_one_thousand.dart
+++ example/lib/view/color_ral_view_one_thousand.dart
@@ -8,7 +8,7 @@ class ColorView1000 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(15.0),
       child: Column(
         children: [
           Wrap(
