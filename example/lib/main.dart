e719108b0e4e5eac9d24c931aa3a883ddce19fb0
maxvyr <maxvid30@gmail.com>
2022-02-23 16:47:44 +0100

    :lipstick: deisgn begins #1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:example/view/color_ral_view.dart';
+import 'package:example/view/color_ral_view_one_thousand.dart';
+import 'package:example/view/color_ral_view_three_thousands.dart';
+import 'package:example/view/color_ral_view_two_thousands.dart';
+import 'package:example/widget/text_presentation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,9 +32,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      padding: EdgeInsets.all(20.0),
-      child: ColorView(),
-    );
+    return SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          shrinkWrap: true,
+          children: const [
+            TextPresentation(title: "RAL 1000"),
+            ColorView1000(),
+            TextPresentation(title: "RAL 2000"),
+            ColorView2000(),
+            TextPresentation(title: "RAL 3000"),
+            ColorView3000(),
+          ],
+        ));
   }
 }
